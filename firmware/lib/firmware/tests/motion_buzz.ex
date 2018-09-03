2be473bd8645bde97ca2d2dc43e0274d69582286
Manuel <>
2018-09-03 23:54:09 +0200

    Retweaked motion_buzz, motion alarm

--- firmware/lib/firmware/tests/motion_buzz.ex
+++ firmware/lib/firmware/tests/motion_buzz.ex
@@ -2,41 +2,40 @@ defmodule Firmware.Tests.MotionBuzz do
   alias Nerves.Grove.Sensor.MotionSensor
   alias Nerves.Grove.Buzzer
 
-  def check_sensor(pin_buzz, pin_sensor) do
+  @type input(buzz_pid, sensor_pid, pin_buzz, pin_sensor) :: [{buzz_pid, sensor_pid, pin_buzz, pin_sensor}]
+  def start_alarm(pin_buzz, pin_sensor) do
     {:ok, buzz_pid} = Buzzer.start_link(pin_buzz)
     {:ok, sensor_pid} = MotionSensor.start_link(pin_sensor)
-
-    if MotionSensor.read(sensor_pid) == false do
-      Buzzer.beep(buzz_pid, 0.25)
-    end
-
-    Process.sleep(250)
-    loop(pin_buzz, pin_sensor)
-    # check_sensor(pin_buzz, pin_sensor, n-1)
+    input = {buzz_pid, sensor_pid, pin_buzz, pin_sensor}
+    self = Task.start(fn ->
+      loop(input)
+    end)
+    self
   end
 
+  def check_sensor(input) do
+
+    if MotionSensor.read(input.sensor_pid) == false do
+      Buzzer.beep(input.buzz_pid, 0.25)
+    end
+  end
+
+  @spec send_stop(pid()) :: pid()
   def send_stop(pid) do
     send(pid, :stop)
   end
 
-  def start_alarm(pin_buzz, pin_sensor) do
-    {:ok, buzz_pid} = Buzzer.start_link(pin_buzz)
-    {:ok, sensor_pid} = MotionSensor.start_link(pin_sensor)
 
-    Task.start(fn ->
-      loop(pin_buzz, pin_sensor)
-    end)
-  end
-
-  def send_stop(pid) do
+  @spec stop(pid()) :: pid()
+  def stop(pid) do
     IO.puts("Shutting down alarm")
     send(pid, :stop)
     Process.exit(pid, :stop)
   end
 
-  def loop(pin_buzz, pin_sensor) do
+  def loop(input) do
     IO.puts("Alarm activated")
-    check_sensor(pin_buzz, pin_sensor)
+    check_sensor(input)
     # Process.sleep(500)
 
     receive do

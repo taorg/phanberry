4a944872974a4768618a90ebffb63f8e3733adf7
iredsausage <antonio.lopez@ptbsl.com>
2018-09-18 18:59:30 +0200

    Could not catch config

--- firmware/lib/firmware/application.ex
+++ firmware/lib/firmware/application.ex
@@ -33,7 +33,7 @@ defmodule Firmware.Application do
 
   def childred(:gpio1602) do
     import Supervisor.Spec, warn: false
-    config = Application.get_env(:hd44780_demo, :lcd)
+    config = Application.get_env(:firmware, :lcd)
     worker(ExLCD, [{ExLCD.GPIO1602, config}])
   end
 end
